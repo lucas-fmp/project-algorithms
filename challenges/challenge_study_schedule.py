44a8963c896ad5c5655c6fd123422b034125e170
Lucas Fernando Macedo Pereira <lucas@pop-os.localdomain>
2023-03-22 16:03:02 -0300

    feat - requirement 1 - implement function challenge_stydy_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) != int or target_time < 0:
+        return None
+
+    count = 0
+    for start, end in permanence_period:
+        if type(start) != int or type(end) != int or start > end:
+            return None
+        if start <= target_time <= end:
+            count += 1
+    return count
