6a1db4efaa793729ea3c5b8342192fa59d9ad2e8
Lucas Fernando Macedo Pereira <lucas@pop-os.localdomain>
2023-03-23 17:19:03 -0300

    feat - requirement 5 - implement function find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(nums=None):
+    if not nums:
+        return False
+
+    nums.sort()
+
+    for index in range(0, len(nums) - 1):
+        if type(nums[index]) == str or nums[index] < 0:
+            return False
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+
+    return False
